c0b475ab3c5aedce31ed3a6b5f2b578ca9892426
AGLOP-1354 <jake@jober.io>
2024-12-22 01:16:00 +0900

    토큰 재발급 및 환경변수 개선

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as Joi from 'joi';
 
 import { AuthModule } from './auth/auth.module';
+import { envVariables } from './common/const/env.config';
 import { DirectorModule } from './director/director.module';
 import { Director } from './director/entity/director.entity';
 import { Genre } from './genre/entities/genre.entity';
@@ -33,12 +34,12 @@ import { UserModule } from './user/user.module';
     }),
     TypeOrmModule.forRootAsync({
       useFactory: (configService: ConfigService) => ({
-        type: configService.get<string>('DB_TYPE') as 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: +configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_DATABASE'),
+        type: configService.get<string>(envVariables.dbType) as 'postgres',
+        host: configService.get<string>(envVariables.dbHost),
+        port: +configService.get<number>(envVariables.dbPort),
+        username: configService.get<string>(envVariables.dbUsername),
+        password: configService.get<string>(envVariables.dbPassword),
+        database: configService.get<string>(envVariables.dbDatabase),
         entities: [Movie, MovieDetail, Director, Genre, User],
         synchronize: true,
       }),
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,15 @@
 import {
-  ConflictException,
+  BadRequestException,
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
+import { envVariables } from 'src/common/const/env.config';
+import { Role, User } from 'src/user/entities/user.entity';
 import { Repository } from 'typeorm';
-
-import { ConfigService } from '@nestjs/config';
-import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -21,88 +21,148 @@ export class AuthService {
   ) {}
 
   parseBasicToken(rawToken: string) {
-    const [type, token] = rawToken.split(' ');
+    const basicSplit = rawToken.split(' ');
 
-    if (type !== 'Basic') {
-      throw new UnauthorizedException('Invalid token type');
+    if (basicSplit.length !== 2) {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
+    }
+
+    const [basic, token] = basicSplit;
+
+    if (basic.toLowerCase() !== 'basic') {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
     }
 
     const decoded = Buffer.from(token, 'base64').toString('utf-8');
+
     const tokenSplit = decoded.split(':');
 
     if (tokenSplit.length !== 2) {
-      throw new UnauthorizedException('Invalid token format');
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
     }
 
     const [email, password] = tokenSplit;
 
-    return { email, password };
+    return {
+      email,
+      password,
+    };
+  }
+
+  async parseBearerToken(rawToken: string, isRefreshToken: boolean) {
+    const basicSplit = rawToken.split(' ');
+
+    if (basicSplit.length !== 2) {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
+    }
+
+    const [bearer, token] = basicSplit;
+
+    if (bearer.toLowerCase() !== 'bearer') {
+      throw new BadRequestException('토큰 포맷이 잘못됐습니다!');
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get<string>(envVariables.refreshTokenSecret),
+      });
+
+      if (isRefreshToken) {
+        if (payload.type !== 'refresh') {
+          throw new BadRequestException('Refresh 토큰을 입력 해주세요!');
+        }
+      } else {
+        if (payload.type !== 'access') {
+          throw new BadRequestException('Access 토큰을 입력 해주세요!');
+        }
+      }
+
+      return payload;
+    } catch (e) {
+      console.log(e);
+      throw new UnauthorizedException('토큰이 만료됐습니다!');
+    }
   }
 
   async register(rawToken: string) {
     const { email, password } = this.parseBasicToken(rawToken);
 
-    const user = await this.userRepository.findOne({ where: { email } });
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
 
     if (user) {
-      throw new ConflictException('User already exists');
+      throw new BadRequestException('이미 가입한 이메일 입니다!');
     }
 
     const hash = await bcrypt.hash(
       password,
-      this.configService.get('HASH_ROUNDS'),
+      this.configService.get<number>(envVariables.hashRounds),
     );
 
-    await this.userRepository.save({ email, password: hash });
+    await this.userRepository.save({
+      email,
+      password: hash,
+    });
 
-    return await this.userRepository.findOne({ where: { email } });
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 
-  async login(rawToken: string) {
-    const { email, password } = this.parseBasicToken(rawToken);
-
-    const user = await this.userRepository.findOne({ where: { email } });
+  async authenticate(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
 
     if (!user) {
-      throw new UnauthorizedException('wrong email');
+      throw new BadRequestException('잘못된 로그인 정보입니다!');
     }
 
     const passOk = await bcrypt.compare(password, user.password);
 
     if (!passOk) {
-      throw new UnauthorizedException('wrong password');
+      throw new BadRequestException('잘못된 로그인 정보입니다!');
     }
 
+    return user;
+  }
+
+  async issueToken(user: { id: number; role: Role }, isRefreshToken: boolean) {
     const refreshTokenSecret = this.configService.get<string>(
-      'REFRESH_TOKEN_SECRET',
+      envVariables.refreshTokenSecret,
     );
     const accessTokenSecret = this.configService.get<string>(
-      'ACCESS_TOKEN_SECRET',
+      envVariables.accessTokenSecret,
     );
 
+    return this.jwtService.signAsync(
+      {
+        sub: user.id,
+        role: user.role,
+        type: isRefreshToken ? 'refresh' : 'access',
+      },
+      {
+        secret: isRefreshToken ? refreshTokenSecret : accessTokenSecret,
+        expiresIn: isRefreshToken ? '24h' : 300,
+      },
+    );
+  }
+
+  async login(rawToken: string) {
+    const { email, password } = this.parseBasicToken(rawToken);
+
+    const user = await this.authenticate(email, password);
+
     return {
-      refreshToken: await this.jwtService.signAsync(
-        {
-          sub: user.id,
-          role: user.role,
-          type: 'refresh',
-        },
-        {
-          secret: refreshTokenSecret,
-          expiresIn: '1d',
-        },
-      ),
-      accessToken: await this.jwtService.signAsync(
-        {
-          sub: user.id,
-          role: user.role,
-          type: 'access',
-        },
-        {
-          secret: accessTokenSecret,
-          expiresIn: 300,
-        },
-      ),
+      refreshToken: await this.issueToken(user, true),
+      accessToken: await this.issueToken(user, false),
     };
   }
 }
