6f0d8d0ff221a01664808e3bc5e052d85d248c9f
AGLOP-1354 <jake@jober.io>
2025-02-02 17:09:36 +0900

    like, unlike count task schedule

--- src/common/tasks.service.ts
+++ src/common/tasks.service.ts
@@ -1,14 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
 import { readdir, unlink } from 'fs/promises';
 import { join, parse } from 'path';
+import { Movie } from 'src/movie/entity/movie.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class TasksService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Movie)
+    private readonly movieRepository: Repository<Movie>,
+  ) {}
 
   logEverySecond() {
     console.log('Every second!');
+  }
+
+  async calculateMovieLikeCounts() {
+    await this.movieRepository.query(
+      `
+        UPDATE movie m SET "likeCount" = (
+          SELECT COUNT(*) FROM movie_user_like mul
+          WHERE m.id = mul."movieId" AND mul."isLike" = true
+        )
+      `,
+    );
+
+    await this.movieRepository.query(
+      `
+        UPDATE movie m SET "dislikeCount" = (
+          SELECT COUNT(*) FROM movie_user_like mul
+          WHERE m.id = mul."movieId" AND mul."isLike" = false
+        )
+      `,
+    );
   }
 
   // @Cron('* * * * * *')
